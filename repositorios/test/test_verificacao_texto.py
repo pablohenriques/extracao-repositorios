9c00e845f472d6979fefd8cc1f680f7f1e2fd69e
Pablo Sousa <ph.info.cont@rede.ulbra.br>
2023-10-05 21:44:19 -0300

    PSI-010 Teste unitario adicionado

--- repositorios/test/test_verificacao_texto.py
+++ repositorios/test/test_verificacao_texto.py
@@ -19,3 +19,25 @@ class TestVerificacaoTexto(unittest.TestCase):
         texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut."
         resultado = consulta_de_palavra_em_texto("psicologia", texto)
         self.assertEquals(resultado, False)
+
+    def test_nao_encontra_palavra_em_lista_de_palavra(self):
+        palavras = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", ".", "In", "ut", "."]
+        resultado = consulta_de_palavra_em_texto("psicologia", palavras)
+        self.assertEquals(resultado, False)
+
+    def test_encontra_palavra_em_lista_de_palavra(self):
+        palavras = ["Lorem", "psicologia", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", ".", "In", "ut", "."]
+        resultado = consulta_de_palavra_em_texto("psicologia", palavras)
+        self.assertEquals(resultado, True)
+
+    def test_encontra_palavra_em_lista_de_palavra_e_texto(self):
+        palavras = ["Lorem", "psicologia", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", ".", "In", "ut", "."]
+        texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non."
+        resultado = consulta_de_palavra_em_texto("psicologia", texto, palavras)
+        self.assertEquals(resultado, True)
+
+    def test_nao_encontra_palavra_em_lista_de_palavra_e_texto(self):
+        palavras = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", ".", "In", "ut", "."]
+        texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non."
+        resultado = consulta_de_palavra_em_texto("psicologia", texto, palavras)
+        self.assertEquals(resultado, False)
