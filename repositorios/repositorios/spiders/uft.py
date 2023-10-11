ef02f6b63c604056be137518416d4e92617faa8f
Pablo Sousa <ph.info.cont@rede.ulbra.br>
2023-10-10 23:21:21 -0300

    PSI-009 Spider atualizado

--- repositorios/repositorios/spiders/uft.py
+++ repositorios/repositorios/spiders/uft.py
@@ -7,4 +7,34 @@ class UftSpider(scrapy.Spider):
     start_urls = ["https://repositorio.uft.edu.br/htmlmap"]
 
     def parse(self, response):
-        pass
+        links = response.css("li a::attr(href)").extract()
+
+        for link in links:
+            yield response.follow(link, callback=self.get_links)
+
+    def get_links(self, response):
+        links = response.css("li a::attr(href)").extract()
+
+        for link in links:
+            yield response.follow(link, callback=self.get_links)
+
+    def get_page(self, response):
+        titulo = response.css("td:nth-child(2).dc_title::text").get()
+        resumo = response.css("td:nth-child(2).dc_description_resumo::text").get()
+        palavras_chave = response.css("td:nth-child(2).dc_subject a::text").getall()
+        url = response.css("div.well code::text").get()
+        tipo = response.css("td.metadataFieldValue a::text").get()
+        data = response.css("td:nth-child(2).dc_date_issued::text").get()
+        autores = response.css("td:nth-child(2).dc_contributor_author::text").getall()
+        repositorio = "UFT"
+
+        yield {
+            "titulo": titulo,
+            "resumo": resumo,
+            "palavras_chave": palavras_chave,
+            "url": url,
+            "tipo": tipo,
+            "data": data,
+            "autores": autores,
+            "repositorio": repositorio
+        }
