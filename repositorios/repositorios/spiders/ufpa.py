eef9cc2618a6a04722d0fe8501e276a0bd7d5c69
Pablo Sousa <ph.info.cont@rede.ulbra.br>
2023-10-10 23:00:29 -0300

    PSI-010 Spider de repositorio atualizado

--- repositorios/repositorios/spiders/ufpa.py
+++ repositorios/repositorios/spiders/ufpa.py
@@ -1,8 +1,5 @@
 import scrapy
 
-from repositorios.items import RepositoriosItem
-from utils.verificacao_texto import consulta_de_palavra_em_texto
-
 
 class UfpaSpider(scrapy.Spider):
     name = "ufpa"
@@ -12,39 +9,23 @@ class UfpaSpider(scrapy.Spider):
     def parse(self, response):
         links_sitemap = response.css("li a::attr(href)").extract()
 
-        for i in range(10):
-            yield scrapy.Request(url=links_sitemap[i], callback=self.get_page_links)
+        for link in links_sitemap:
+            yield scrapy.follow(link, callback=self.get_page_links)
 
     def get_page_links(self, response):
         links_page = response.css("li a::attr(href)").extract()
 
-        for i in range(len(links_page)):
-            yield scrapy.Request(url=links_page[i], callback=self.open_link)
-
-    def open_link(self, response):
-        yield scrapy.Request(url=str(response), callback=self.get_details)
+        for link in links_page:
+            yield scrapy.follow(link, callback=self.get_details)
 
     def get_details(self, response):
-        try:
-            url = response.css("div.well code::text").get()
-            tipo = response.css("td.dc_type a::text").get()
-            data = response.css("td:nth-child(2).dc_date_issued::text").get()
-            autores = response.css("td.dc_creator a::text").getall()
-            titulo = response.css("td:nth-child(2).dc_title::text").get()
-            resumo = response.css("td:nth-child(2).dc_description_resumo::text").get()
-            palavras_chave = response.css("td:nth-child(2).dc_subject a::text").getall()
-            repositorio = "UFPA"
+        url = response.css("div.well code::text").get()
+        tipo = response.css("td.dc_type a::text").get()
+        data = response.css("td:nth-child(2).dc_date_issued::text").get()
+        autores = response.css("td.dc_creator a::text").getall()
+        titulo = response.css("td:nth-child(2).dc_title::text").get()
+        resumo = response.css("td:nth-child(2).dc_description_resumo::text").get()
+        palavras_chave = response.css("td:nth-child(2).dc_subject a::text").getall()
+        repositorio = "UFPA"
 
-            if consulta_de_palavra_em_texto(titulo, resumo, titulo, palavras_chave):
-                documento = RepositoriosItem()
-                documento["url"] = url
-                documento["tipo"] = tipo
-                documento["data"] = data
-                documento["autores"] = autores
-                documento["titulo"] = titulo
-                documento["resumo"] = resumo
-                documento["palavras_chave"] = palavras_chave
-                documento["repositorio"] = repositorio
-                yield documento
-        except Exception as e:
-            self.log(e)
+
