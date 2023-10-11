2fa066c4d364a5b6122b82b063975bf4be75864a
Pablo Sousa <ph.info.cont@rede.ulbra.br>
2023-10-10 23:00:10 -0300

    PSI-010 Atualizacao de configuracoes

--- repositorios/repositorios/settings.py
+++ repositorios/repositorios/settings.py
@@ -66,9 +66,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   "repositorios.pipelines.RepositoriosPipeline": 300,
-}
+# ITEM_PIPELINES = {
+#    "repositorios.pipelines.RepositoriosPipeline": 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
